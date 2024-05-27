75bda056b432f7822b7ee5ccf3e49f3e2d94f690
victorhanert <vict660m@stud.kea.dk>
2024-05-27 12:06:32 +0200

    Delete list

--- client/src/components/Profile/List.svelte
+++ client/src/components/Profile/List.svelte
@@ -1,11 +1,14 @@
 <script>
   import { BASE_URL } from "../../stores/generalStore.js";
-  import { Button, Modal } from "flowbite-svelte";
+  import { Button, Modal, Popover } from "flowbite-svelte";
   import { CirclePlusSolid, TrashBinSolid } from "flowbite-svelte-icons";
   import { userStore } from "../../stores/authStore";
-  import { fetchPost } from "../../util/api";
+  import { fetchPost, fetchDelete } from "../../util/api";
 
   let openCreateListModal = false;
+
+  export let lists = [];
+  export let isOwner;
 
   async function handleCreateList(event) {
     event.preventDefault();
@@ -19,13 +22,19 @@
       payload
     );
     if (response.status === 201) {
-      lists = response.data.data;
+      lists = [...lists, response.data.data]; // Update lists with the new list
+      openCreateListModal = false; // Close the modal after successful creation
     }
-    openCreateListModal = false;
   }
 
-  export let isOwner;
-  export let lists = [];
+  async function handleDeleteList(listId) {
+    const response = await fetchDelete(
+      `${$BASE_URL}/api/lists/${$userStore.id}/${listId}`
+    );
+    if (response.status === 200) {
+      lists = lists.filter((list) => list.id !== listId);
+    }
+  }
 </script>
 
 <div class="flex items-center">
@@ -40,7 +49,9 @@
 {/if}
 
 {#each lists as list}
-  <div class="flex justify-between bg-white shadow-md rounded-lg p-4 my-4 hover:bg-primary-100 cursor-pointer">
+  <div
+    class="flex justify-between bg-white shadow-md rounded-lg p-4 my-4 hover:bg-primary-100 cursor-pointer"
+  >
     <div class="content">
       <h3 class="text-slate-900 text-xl">
         {list.list_name} - <b>{list.username}</b>
@@ -50,17 +61,31 @@
           class="w-5 h-5 rounded-full inline-block"
         />
       </h3>
-      <p class="text-sm">{list.description} - <b>{list.movie_count} movies</b></p>
+      <p class="text-sm">
+        {list.description} - <b>{list.movie_count} movies</b>
+      </p>
     </div>
     {#if isOwner}
       <div class="actions flex flex-col items-end">
-          <button class="bg-green-800 hover:bg-green-900 text-white rounded-md"><CirclePlusSolid /></button>
-          <button class="bg-red-700 hover:bg-red-800 text-white rounded-md"><TrashBinSolid /></button>
+        <button
+          id="addMovies"
+          class="bg-green-800 hover:bg-green-900 text-white rounded-md py-1">
+          <CirclePlusSolid /></button>
+        <Popover class="w-64 text-sm font-light" triggeredBy="#addMovies">
+          Click here to add movies to this list
+        </Popover>
+        <button
+          on:click={() => handleDeleteList(list.id)}
+          id="deleteList"
+          class="bg-red-700 hover:bg-red-800 text-white rounded-md mt-1 py-1">
+          <TrashBinSolid /></button>
+        <Popover class="w-64 text-sm font-light" triggeredBy="#deleteList">
+          Click here to delete this list
+        </Popover>
       </div>
     {/if}
   </div>
 {/each}
-
 
 <Modal
   title="Create a new list"
@@ -94,7 +119,6 @@
     <button
       class="hover:bg-primary-800 bg-primary-600 text-white mt-4"
       type="submit"
-      on:click={() => (openCreateListModal = false)}
     >
       Create new list
     </button>
